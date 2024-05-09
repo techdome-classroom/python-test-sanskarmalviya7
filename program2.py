16e89be3e2ed46b78b1c36314ffebedd96f379c3
Sanskar Malviya <92670924+sanskarmalviya7@users.noreply.github.com>
2024-05-09 13:13:45 +0530

    Add files via upload

--- program2.py
+++ program2.py
@@ -1,9 +1,25 @@
-def decode_message( s: str, p: str) -> bool:
-    if p == '?b?':
-        return True
-    elif p == '*':
-        return True
-    elif p != s:
+def decode_message(s: str, p: str) -> bool:
+    message_index = 0
+    key_index = 0
+    while message_index < len(s) and key_index < len(p):
+        if p[key_index] == '*':
+            
+            while message_index < len(s) and p[key_index] == '*':
+                message_index += 1
+            key_index += 1
+        elif p[key_index] == '?':
+           
+            message_index += 1
+            key_index += 1
+        elif s[message_index] != p[key_index]:
+           
+            return False
+        else:
+            message_index += 1
+            key_index += 1
+    
+    
+    if message_index < len(s) or key_index < len(p):
         return False
     
     return True
